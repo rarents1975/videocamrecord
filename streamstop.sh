d2329bf6d6401d8968b7ba0ab938bce2d86531d5
Ralf Rentschler <ralf.rentschler@gmx.net>
2018-11-03 17:33:11 +0100

    Create streamstop.sh

---
+++ streamstop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Dieses Script schaltet folgendes aus:
+# - Livestream, Lokale Aufnahme und Anzeige des Kamerabildes auf dem Raspiscreen
+# - Anzeige des Kamerabildes auf dem Raspiscreen
+
+# Gstreamer Prozesse beenden:
+x=99
+x=$(pidof gst-launch-1.0)
+echo $x
+sudo kill $x
+
+# omxplayer Prozesse beenden
+y=99
+y=$(ps -ef | grep 'startOMXPlayer'  | grep -v grep | awk '{print $2}')
+echo "y= "$y
+sudo kill $y
